975150a5c6c3c059d0b643fd5b034ed473769378
duongdv95 <danielvanduong@gmail.com>
2018-04-26 23:06:54 +0000

    node js practice

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,8 +2,14 @@ console.log("Starting app..");
 
 const fs = require("fs");
 const os = require("os");
+const _ = require("lodash");
 const notes = require("./notes.js");
 
-var user = os.userInfo();
+// console.log(_.isString(true));
+// console.log(_.isString("Andrew"));
+var filteredArray = _.uniq(["Bob", 1, "Daniel", 1])
+console.log(filteredArray);
+// console.log(notes.add(5,6));
+// var user = os.userInfo();
 
-fs.appendFile("greetings.txt", `Hellur ${user.username}!`);
+// fs.appendFile("greetings.txt", `Hellur ${user.username}! You are ${notes.age}`);
--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,1 +1,5 @@
 console.log("Starting node.js");
+
+module.exports.add = (a, b) => {
+    return a + b;
+}
