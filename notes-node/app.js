cd7a7da2fd18cc40ce10a8b804edb1275da9a85c
duongdv95 <danielvanduong@gmail.com>
2018-04-27 10:08:21 +0000

    adding and saving notes

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,7 +1,7 @@
 console.log("Starting app..");
 
-const fs = require("fs");
-const _ = require("lodash");
+const fs    = require("fs");
+const _     = require("lodash");
 const yargs = require("yargs");
 const notes = require("./notes.js");
 
--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,7 +1,20 @@
+const fs = require("fs");
 console.log("Starting node.js");
 
 var addNote = (title, body) => {
-    console.log("Adding note", title, body);
+    var notes = [];
+    var note = {title, body};
+    notes.push(note);
+    var oldData = fs.readFileSync("notes-data.json");
+    
+    if(!oldData) {
+        var notesString = JSON.stringify(notes);
+    } else {
+        var oldDataArray = JSON.parse(oldData);
+        var combineData = oldDataArray.concat(notes);
+        var notesString = JSON.stringify(combineData);
+    }
+    fs.writeFileSync("notes-data.json", notesString);
 };
 
 var getAll = () => {
