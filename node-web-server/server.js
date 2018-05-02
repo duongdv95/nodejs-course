3527de7e609e4eda10742ae60892a67389ab0632
duongdv95 <danielvanduong@gmail.com>
2018-05-02 10:27:41 +0000

    handlebars templating exercise

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,21 +1,30 @@
 const express = require("express");
+const hbs     = require("hbs");
 
 var app = express();
 
+hbs.registerPartials(__dirname + "/views/partials");
 app.use(express.static(__dirname + "/public"));
+app.set("view engine", "hbs");
+
+hbs.registerHelper("getCurrentYear", () => {
+    return new Date().getFullYear();
+})
+
+hbs.registerHelper("screamIt", (text) => {
+    return text.toUpperCase();
+})
 app.get("/", (req, res) => {
-    res.send({
-        name: "Daniel",
-        likes: [
-            "Fishing",
-            "Hiking",
-            "Camping"
-        ]
-    });
+    res.render("home", {
+        pageTitle: "Home Page",
+        welcomeMessage: "Welcome to my website!"
+    })
 })
 
 app.get("/about", (req,res) => {
-    res.send("About Page");
+    res.render("about", {
+        pageTitle: "About Page"
+    });
 });
 
 app.get("/bad", (req,res) => {
