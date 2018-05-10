b1ad87968536576682adfd0d5475c4fe752fb9a3
duongdv95 <danielvanduong@gmail.com>
2018-05-10 10:27:22 +0000

    Setup separate test and development envs

--- node-todo-api/server/db/mongoose.js
+++ node-todo-api/server/db/mongoose.js
@@ -1,8 +1,9 @@
 var mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://duongdv:12345@ds119350.mlab.com:19350/todoapp" || "mongodb://localhost:27017/TodoApp");
-
+mongoose.connect(process.env.MONGODB_URI);
+//"mongodb://duongdv:12345@ds119350.mlab.com:19350/todoapp"
 module.exports = {
     mongoose
 }
+
