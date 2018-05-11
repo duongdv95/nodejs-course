b5dc3a7298c634e9a77aad88626517458cea61e6
duongdv95 <danielvanduong@gmail.com>
2018-05-11 11:21:45 +0000

    Add POST /users route and token method

--- node-todo-api/server/models/user.js
+++ node-todo-api/server/models/user.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const jwt = require("jsonwebtoken");
+const _ = require("lodash");
 
-var User = mongoose.model("User", {
+var Schema = mongoose.Schema;
+var UserSchema = new Schema({
    email: {
        type: String,
        required: true,
@@ -9,12 +12,44 @@ var User = mongoose.model("User", {
        minlength: 1,
        unique: true,
        validate: {
-           validator: (value) => {
-               
-           },
+           validator: validator.isEmail,
            message: "{VALUE} is not a valid email"
        }
-   }
-});
+   },
+   password: {
+       type: String,
+       require: true,
+       minLength: 6
+   },
+   tokens: [{
+       access: {
+           type: String,
+           required: true
+       },
+       token: {
+           type: String,
+           required: true
+       }
+   }]
+})
+
+UserSchema.methods.toJSON = function () {
+    var user = this;
+    var userObject = user.toObject();
+    
+    return _.pick(userObject, ["_id", "email"]);
+}
+UserSchema.methods.generateAuthToken = function () {
+    var user = this;
+    var access = "auth";
+    var token = jwt.sign({_id: user._id.toHexString(), access}, "abc123").toString();
+    
+    user.tokens.concat([{access, token}])
+    
+    return user.save().then(() => {
+        return token;
+    });
+}
+var User = mongoose.model("User", UserSchema);
 
 module.exports = {User};
--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -117,12 +117,14 @@ app.patch("/todos/:id", (req, res) => {
 })
 
 // POST /users
-app.get("/users", (req, res) => {
+app.post("/users", (req, res) => {
     var body = _.pick(req.body, ["email", "password"]);
     var user = new User(body);
     
-    user.save().then((doc) => {
-        res.send(doc);
+    user.save().then(() => {
+        return user.generateAuthToken();
+    }).then((token) => {
+        res.header("x-auth", token).send(user);
     }).catch((e) => {
         res.status(400).send(e);
     });
