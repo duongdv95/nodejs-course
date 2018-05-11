784ed1ad0ec2c860efe6329bc7b0f427f3c7f81d
duongdv95 <danielvanduong@gmail.com>
2018-05-11 08:41:07 +0000

    Update user model and create POST /users route

--- node-todo-api/server/models/user.js
+++ node-todo-api/server/models/user.js
@@ -1,11 +1,19 @@
-var mongoose = require("mongoose");
+const mongoose = require("mongoose");
+const validator = require("validator");
 
 var User = mongoose.model("User", {
    email: {
        type: String,
        required: true,
+       trim: true,
        minlength: 1,
-       trim: true
+       unique: true,
+       validate: {
+           validator: (value) => {
+               
+           },
+           message: "{VALUE} is not a valid email"
+       }
    }
 });
 
--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -116,6 +116,18 @@ app.patch("/todos/:id", (req, res) => {
     })
 })
 
+// POST /users
+app.get("/users", (req, res) => {
+    var body = _.pick(req.body, ["email", "password"]);
+    var user = new User(body);
+    
+    user.save().then((doc) => {
+        res.send(doc);
+    }).catch((e) => {
+        res.status(400).send(e);
+    });
+});
+
 app.listen(port, process.env.IP, () => {
     console.log(`Started up at port ${port}`);
 });
