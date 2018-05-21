df910008e3b390fb5b7abd4a6b97ad8b7edbc9d2
duongdv95 <danielvanduong@gmail.com>
2018-05-21 02:57:45 +0000

    update config to use config.json

--- node-todo-api/server/config/config.js
+++ node-todo-api/server/config/config.js
@@ -1,9 +1,10 @@
 var env = process.env.NODE_ENV || "development";
 
-if(env === "development") {
-    process.env.MONGODB_URI = "mongodb://localhost:27017/TodoApp"
-} else if (env === "test") {
-    process.env.MONGODB_URI = "mongodb://localhost:27017/TodoAppTest"
-} else if (env === "production") {
-    process.env.MONGODB_URI = "mongodb://duongdv:12345@ds119350.mlab.com:19350/todoapp"
+if (env === "development" || env === "test" || env === "production") {
+    var config = require("./config.json");
+    var envConfig = config[env];
+    
+    Object.keys(envConfig).forEach((key) => {
+        process.env[key] = envConfig[key];
+    });
 }
