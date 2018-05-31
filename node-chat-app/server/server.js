fa4d0f349c38cf922f195e0122edd9ccad3cfdd6
duongdv95 <danielvanduong@gmail.com>
2018-05-31 02:44:04 +0000

    node-chat-app first commit

---
+++ node-chat-app/server/server.js
@@ -0,0 +1,14 @@
+const path = require("path");
+const express = require("express");
+
+
+const publicPath = path.join(__dirname, "../public");
+const port = process.env.PORT
+
+var app = express();
+
+app.use(express.static(publicPath));
+
+app.listen(port, process.env.IP, () => {
+    console.log("server connected")
+})
