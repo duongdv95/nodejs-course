0fec23cbe2e9a7f899c8533659a0c04f8bae855b
duongdv95 <danielvanduong@gmail.com>
2018-06-19 19:51:17 +0000

    Add form for messages and show incoming messages in browser

--- node-chat-app/public/js/index.js
+++ node-chat-app/public/js/index.js
@@ -11,4 +11,17 @@ socket.on("disconnect", function () {
 
 socket.on("newMessage", function (message) {
     console.log("newMessage", message);
+    var li = document.createElement("li");
+    li.innerHTML = `${message.from}: ${message.text}`
+    document.getElementById("messages").appendChild(li);
 })
+
+document.getElementById("message-form").addEventListener("submit", function (e) {
+    e.preventDefault();
+    socket.emit("createMessage", {
+        from: "User",
+        text: document.getElementsByName("message")[0].value
+    }, function () {
+        
+    });
+});
